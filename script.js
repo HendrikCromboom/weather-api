8c144ee6fcda911cb357c344fb668a328311f4bb
HENDRIK <hendrik@KALI.home>
2020-09-16 10:36:55 +0200

    added country input, fetching longitude and latitude

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+function getId(id) { return document.getElementById(id); }
+function returnValue(id) { return getId(id).value; }
+function resetValue(id) { getId(id).value = ""; }
 var library = {
     city: "",
-    country: ""
+    country: "",
+    lat: "",
+    lon: ""
 };
+function formSubmit() {
+    getInputFields();
+}
+function getInputFields() {
+    library.city = returnValue("inputFieldCity");
+    library.country = returnValue("inputFieldCountry");
+    resetValue("inputFieldCity");
+    resetValue("inputFieldCountry");
+    getForecast();
+}
 function getForecast() {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country + "&appid=a22242314b0694ba3aa5480b6172d174")
         .then(function (response) { return response.json(); })
         .then(function (data) {
+        library.lon = data["city"]["coord"].lon;
+        library.lat = data["city"]["coord"].lat;
     });
 }
--- script.ts
+++ script.ts
@@ -1,15 +1,37 @@
+function getId(id){ return document.getElementById(id)}
+function returnValue(id){ return (<HTMLInputElement>getId(id)).value}
+function resetValue(id){(<HTMLInputElement>getId(id)).value = ""}
+
 let library : {
     city: string,
-    country: string
+    country: string,
+    lat: string,
+    lon: string,
 }={
     city: "",
-    country: ""
+    country: "",
+    lat: "",
+    lon: ""
+}
+
+function formSubmit(){
+    getInputFields();
+
+}
+function getInputFields(){
+    library.city = returnValue("inputFieldCity");
+    library.country = returnValue("inputFieldCountry");
+    resetValue("inputFieldCity")
+    resetValue("inputFieldCountry")
+    getForecast();
 }
 
 function getForecast() {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country +"&appid=a22242314b0694ba3aa5480b6172d174")
         .then(response => response.json())
         .then(data => {
+                    library.lon = data["city"]["coord"].lon
+                    library.lat = data["city"]["coord"].lat
 
         })
 }
