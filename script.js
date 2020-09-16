ca11e1e96c9206dcbab7353fb1f2db8e2f99b88e
HENDRIK <hendrik@KALI.home>
2020-09-16 11:15:20 +0200

    added inputfield for key, parsing days of week

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function resetValue(id) { getId(id).value = ""; }
 var library = {
     city: "",
     country: "",
+    key: "",
     lat: "",
-    lon: ""
+    lon: "",
+    today: [],
+    days: [0, 0, 0, 0, 0, 0]
 };
 function formSubmit() {
     getInputFields();
@@ -13,15 +16,25 @@ function formSubmit() {
 function getInputFields() {
     library.city = returnValue("inputFieldCity");
     library.country = returnValue("inputFieldCountry");
+    library.key = returnValue("inputFieldKey");
     resetValue("inputFieldCity");
     resetValue("inputFieldCountry");
     getForecast();
 }
 function getForecast() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country + "&appid=a22242314b0694ba3aa5480b6172d174")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country + "&appid=" + library.key)
         .then(function (response) { return response.json(); })
         .then(function (data) {
         library.lon = data["city"]["coord"].lon;
         library.lat = data["city"]["coord"].lat;
+        library.days.forEach(function (day, i) {
+            library.today.push(data["list"][i]["main"].temp);
+        });
     });
 }
+/*function getOneCall() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + library.lat + "&lon=" + library.lon + "&appid=a22242314b0694ba3aa5480b6172d174")
+        .then(response => response.json())
+        .then(data => {
+        })
+}*/ 
--- script.ts
+++ script.ts
@@ -5,13 +5,19 @@ function resetValue(id){(<HTMLInputElement>getId(id)).value = ""}
 let library : {
     city: string,
     country: string,
+    key: string,
     lat: string,
     lon: string,
+    today: number[],
+    days: number[]
 }={
     city: "",
     country: "",
+    key: "",
     lat: "",
-    lon: ""
+    lon: "",
+    today: [],
+    days: [0,0,0,0,0,0]
 }
 
 function formSubmit(){
@@ -21,17 +27,28 @@ function formSubmit(){
 function getInputFields(){
     library.city = returnValue("inputFieldCity");
     library.country = returnValue("inputFieldCountry");
+    library.key = returnValue("inputFieldKey")
     resetValue("inputFieldCity")
     resetValue("inputFieldCountry")
     getForecast();
 }
 
 function getForecast() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country +"&appid=a22242314b0694ba3aa5480b6172d174")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country +"&appid="+library.key)
         .then(response => response.json())
         .then(data => {
                     library.lon = data["city"]["coord"].lon
                     library.lat = data["city"]["coord"].lat
-
+                    library.days.forEach((day, i)=>{
+                        library.today.push(data["list"][i]["main"].temp)
+                        }
+                    )
         })
 }
+
+/*function getOneCall() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + library.lat + "&lon=" + library.lon + "&appid=a22242314b0694ba3aa5480b6172d174")
+        .then(response => response.json())
+        .then(data => {
+        })
+}*/
