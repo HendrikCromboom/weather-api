f0f7777c42ae5f6925a12e45d9974eaacf29addc
HENDRIK <hendrik@KALI.home>
2020-09-16 15:22:50 +0200

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Classes to shorthand DOM manipulation
 function getId(id) { return document.getElementById(id); }
 function returnValue(id) { return getId(id).value; }
 function resetValue(id) { getId(id).value = ""; }
+// OOP Constructor to simplify, reset and preset the data
 var library = {
     city: "",
     country: "",
@@ -12,31 +14,39 @@ var library = {
     sixDayIcon: [],
     days: [0, 0, 0, 0, 0, 0]
 };
+// Function that gets called by the inline HTML to prevent default reload
 function formSubmit() {
     getInputFields();
 }
+// Pulling the input values from the DOM
 function getInputFields() {
     library.city = returnValue("inputFieldCity");
     library.country = returnValue("inputFieldCountry");
     library.key = returnValue("inputFieldKey");
+    //Resetting the input fields
     resetValue("inputFieldCity");
     resetValue("inputFieldCountry");
+    //Calls the API function, will setup a class later
     getForecast();
 }
+// Fetching the data from the API
 function getForecast() {
+    // The &units turns the returned api to degrees C instead of degrees F
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country + "&units=metric&appid=" + library.key)
         .then(function (response) { return response.json(); })
         .then(function (data) {
         console.log(data);
+        //longitude and latitude for testing purposes
         library.lon = data["city"]["coord"].lon;
         library.lat = data["city"]["coord"].lat;
+        //For each day that needs to be displayed we loop over the array of 40 days and return the objects that we need
         library.days.forEach(function (day, i) {
-            library.sixDayTemp.push(data["list"][i]["main"].temp);
-            library.sixDayWeather.push(data["list"][i]["weather"][i].main);
-            library.sixDayIcon.push(data["list"][i]["weather"][i].icon);
+            library.sixDayTemp.push(data["list"][i]["main"].temp); //This stores the average temperature in an array of 6 days
+            library.sixDayWeather.push(data["list"][i]["weather"][i].main); //This stores the named weather in an array of 6 days
+            library.sixDayIcon.push(data["list"][i]["weather"][i].icon); //This stores the icon name  in an array of 6 days
         });
     })["catch"](function (error) {
-        console.log(error);
+        console.log(error); // Catches any errors regarding the fetch -> the fetch is a promise and requires a valid XML input
     });
 }
 //a22242314b0694ba3aa5480b6172d174
--- script.ts
+++ script.ts
@@ -1,7 +1,8 @@
+// Classes to shorthand DOM manipulation
 function getId(id){ return document.getElementById(id)}
 function returnValue(id){ return (<HTMLInputElement>getId(id)).value}
 function resetValue(id){(<HTMLInputElement>getId(id)).value = ""}
-
+// OOP Constructor to simplify, reset and preset the data
 let library : {
     city: string,
     country: string,
@@ -23,35 +24,40 @@ let library : {
     sixDayIcon: [],
     days: [0,0,0,0,0,0]
 }
-
+// Function that gets called by the inline HTML to prevent default reload
 function formSubmit(){
     getInputFields();
-
 }
+// Pulling the input values from the DOM
 function getInputFields(){
     library.city = returnValue("inputFieldCity");
     library.country = returnValue("inputFieldCountry");
     library.key = returnValue("inputFieldKey")
+    //Resetting the input fields
     resetValue("inputFieldCity")
     resetValue("inputFieldCountry")
+    //Calls the API function, will setup a class later
     getForecast();
 }
-
+// Fetching the data from the API
 function getForecast() {
+    // The &units turns the returned api to degrees C instead of degrees F
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + library.city + "," + library.country +"&units=metric&appid="+library.key)
         .then(response => response.json())
         .then(data => {
             console.log(data)
+                    //longitude and latitude for testing purposes
                     library.lon = data["city"]["coord"].lon
                     library.lat = data["city"]["coord"].lat
+                    //For each day that needs to be displayed we loop over the array of 40 days and return the objects that we need
                     library.days.forEach((day, i)=>{
-                        library.sixDayTemp.push(data["list"][i]["main"].temp)
-                        library.sixDayWeather.push(data["list"][i]["weather"][i].main)
-                        library.sixDayIcon.push(data["list"][i]["weather"][i].icon)
+                        library.sixDayTemp.push(data["list"][i]["main"].temp) //This stores the average temperature in an array of 6 days
+                        library.sixDayWeather.push(data["list"][i]["weather"][i].main) //This stores the named weather in an array of 6 days
+                        library.sixDayIcon.push(data["list"][i]["weather"][i].icon) //This stores the icon name  in an array of 6 days
                         })
         })
         .catch(error=>{
-            console.log(error)
+            console.log(error) // Catches any errors regarding the fetch -> the fetch is a promise and requires a valid XML input
         })
 }
 
